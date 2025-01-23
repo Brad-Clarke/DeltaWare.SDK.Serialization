8711cde51d99d3bd85cdfb7147ab82869bf13993
Bradley Clarke <bradley.clarke@outlook.com>
2025-01-23 21:13:00 +1100

    Commt

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,12 @@
-﻿using ConsoleApp1.Excel;
+﻿using CsvHelper.Configuration.Attributes;
 using DeltaWare.SDK.Serialization.Csv;
 using DeltaWare.SDK.Serialization.Csv.Attributes;
 using DeltaWare.SDK.Serialization.Csv.Reading;
 using DeltaWare.SDK.Serialization.Csv.Reading.Options;
 using DeltaWare.SDK.Serialization.Csv.Writing;
 using System.Diagnostics;
-using static ConsoleApp1.MobileServiceModel;
+using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace ConsoleApp1
 {
@@ -15,31 +16,19 @@ namespace ConsoleApp1
         {
             Console.ReadKey();
 
-            var items = await ProfileCsv();
-
-            string filePath = @"C:\Users\BradleyClarke\Downloads\test_output.csv";
-
-            var stream = File.Create(filePath);
-
-            CsvStreamWriter write = new CsvStreamWriter(new StreamWriter(stream));
-
-            CsvSerializer serializer = new CsvSerializer();
-
-            Stopwatch stopwatch = Stopwatch.StartNew();
-
-            await serializer.SerializeAsync(items, write, false);
-
-            stopwatch.Stop();
-
-            Console.WriteLine($"CSV Write       | Rows: {items.Count}     | Time: {stopwatch.Elapsed.TotalMilliseconds}");
-
-            await write.FlushAsync();
-            await stream.DisposeAsync();
+            await ProfileCsv();
+            await ProfileCsvHelper();
+            await ProfileCsv();
+            await ProfileCsvHelper();
+            await ProfileCsv();
+            await ProfileCsvHelper();
+            await ProfileCsv();
+            await ProfileCsvHelper();
 
             Console.ReadKey();
         }
 
-        private static async Task<IReadOnlyList<MobileCustomerModel>> ProfileCsv()
+        private static async Task ProfileCsv()
         {
             string filePath = @"C:\Users\BradleyClarke\Downloads\test_big.csv";
 
@@ -61,28 +50,28 @@ namespace ConsoleApp1
 
             stopwatch.Stop();
 
-            Console.WriteLine($"CSV  Read       | Rows: {items.Count}     | Time: {stopwatch.Elapsed.TotalMilliseconds}");
+            Console.WriteLine($"CSV              | Rows: {items.Count}     | Time: {stopwatch.Elapsed.TotalMilliseconds}");
 
             await stream.DisposeAsync();
-
-            return items;
         }
 
-        private static async Task ProfileExcel()
+        private static async Task ProfileCsvHelper()
         {
-            var filePath = @"C:\Users\BradleyClarke\Downloads\test.xlsx";
-
-            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            string filePath = @"C:\Users\BradleyClarke\Downloads\test_big.csv";
 
             var stream = File.Open(filePath, FileMode.Open);
 
-            var stopwatch = Stopwatch.StartNew();
+            var config = CsvConfiguration.FromAttributes<MobileCustomerModel>();
 
-            var items = ExcelDeserializer.Deserialize<MobileServiceModel>(stream).ToList();
+            CsvReader reader = new CsvReader(new StreamReader(stream), config);
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            var items = await reader.GetRecordsAsync<MobileCustomerModel>().ToListAsync();
 
             stopwatch.Stop();
 
-            Console.WriteLine($"Excel     | Rows: {items.Count}     | Time: {stopwatch.Elapsed.TotalMilliseconds}");
+            Console.WriteLine($"CSV Helper       | Rows: {items.Count}     | Time: {stopwatch.Elapsed.TotalMilliseconds}");
 
             await stream.DisposeAsync();
         }
@@ -103,141 +92,90 @@ namespace ConsoleApp1
         }
     }
 
-    public class MobileServiceModel
+    public class MobileCustomerModel
     {
+        [Ignore]
         [CsvIgnore]
         public Ulid Id { get; set; }
 
+        [Ignore]
         [CsvIgnore]
-        public string? PinpointSubUserId { get; set; }
+        public string? PinpointUserId { get; set; }
 
+        [Ignore]
         [CsvIgnore]
-        public Ulid? BurpFamilyMemberId { get; set; }
+        public Ulid? GuardianUserId { get; set; }
 
-        [CsvIgnore]
-        public Ulid? OctaneCustomerId { get; set; }
-
+        [Ignore]
         [CsvIgnore]
         public int LineNo { get; set; }
 
-        [CsvIndex(2)]
+        [Index(0)]
+        [CsvIndex(0)]
         public int CustomerNumber { get; set; }
 
-        [CsvIndex(3)]
-        public string ServiceNumber { get; set; }
+        [Ignore]
+        [CsvIgnore]
+        public string Company { get; set; }
 
-        [CsvIndex(4)]
-        public DateTimeOffset? DateReleased { get; set; }
-
-        [CsvIndex(7)]
+        [Index(29)]
+        [CsvIndex(29)]
         public DateTimeOffset DateAdded { get; set; }
 
-        [CsvIndex(8)]
-        public DateTimeOffset ProRataDate { get; set; }
+        [Index(18)]
+        [CsvIndex(18)]
+        public string Address { get; set; }
 
+        [Index(19)]
+        [CsvIndex(19)]
+        public string Address2 { get; set; }
+
+        [Index(20)]
+        [CsvIndex(20)]
+        public string Suburb { get; set; }
+
+        [Index(22)]
+        [CsvIndex(22)]
+        public string State { get; set; }
+
+        [Index(21)]
+        [CsvIndex(21)]
+        public string PostCode { get; set; }
+
+        [Index(7)]
+        [CsvIndex(7)]
+        public string ContactPhoneNumber { get; set; }
+
+        [Index(9)]
         [CsvIndex(9)]
-        public DateTimeOffset DateProvisioned { get; set; }
+        public string ContactMobile { get; set; }
 
+        [Index(10)]
         [CsvIndex(10)]
-        public DateTimeOffset? DateDisconnected { get; set; }
+        public string ContactFax { get; set; }
 
+        [Index(11)]
         [CsvIndex(11)]
-        public ServiceStatuses ServiceStatus { get; set; }
+        public string ContactEmail { get; set; }
 
-        [CsvIndex(12)]
-        public string RetailPlanOffer { get; set; }
+        [Index(5)]
+        [CsvIndex(5)]
+        public string ContactFirstname { get; set; }
 
-        [CsvIndex(13)]
-        public DateTimeOffset LastRechargeDate { get; set; }
+        [Index(6)]
+        [CsvIndex(6)]
+        public string ContactSurname { get; set; }
 
-        [CsvIndex(14)]
-        public string WholesalePlanOffer { get; set; }
+        [Index(36)]
+        [CsvIndex(36)]
+        public string CreditStatus { get; set; }
 
-        [CsvIndex(17)]
-        public string? IMSI { get; set; }
+        [Index(37)]
+        [CsvIndex(37)]
+        public DateTimeOffset DateUpdated { get; set; }
 
-        [CsvIgnore]
-        public int ExpiryDays => this.RetailPlanOffer.StartsWith("365") ? 365 : this.RetailPlanOffer.StartsWith("180") ? 180 : 30;
-
-        [CsvIgnore]
-        public bool Active => ServiceStatus == ServiceStatuses.OK;
-
-        [CsvIgnore]
-        public bool Stale => ServiceStatus == ServiceStatuses.OK && DateTimeOffset.UtcNow.Subtract(LastRechargeDate).TotalDays <= ExpiryDays;
-
-        public enum ServiceStatuses
-        {
-            Disconnected = 0,
-            OK = 1,
-        }
-
-        public class MobileCustomerModel
-        {
-            [CsvIgnore]
-            public Ulid Id { get; set; }
-
-            [CsvIgnore]
-            public string? PinpointUserId { get; set; }
-
-            [CsvIgnore]
-            public Ulid? GuardianUserId { get; set; }
-
-            [CsvIgnore]
-            public int LineNo { get; set; }
-
-            [CsvIndex(0)]
-            public int CustomerNumber { get; set; }
-
-            [CsvIgnore]
-            public string Company { get; set; }
-
-            [CsvIndex(29)]
-            public DateTimeOffset DateAdded { get; set; }
-
-            [CsvIndex(18)]
-            public string Address { get; set; }
-
-            [CsvIndex(19)]
-            public string Address2 { get; set; }
-
-            [CsvIndex(20)]
-            public string Suburb { get; set; }
-
-            [CsvIndex(22)]
-            public string State { get; set; }
-
-            [CsvIndex(21)]
-            public string PostCode { get; set; }
-
-            [CsvIndex(7)]
-            public string ContactPhoneNumber { get; set; }
-
-            [CsvIndex(9)]
-            public string ContactMobile { get; set; }
-
-            [CsvIndex(10)]
-            public string ContactFax { get; set; }
-
-            [CsvIndex(11)]
-            public string ContactEmail { get; set; }
-
-            [CsvIndex(5)]
-            public string ContactFirstname { get; set; }
-
-            [CsvIndex(6)]
-            public string ContactSurname { get; set; }
-
-            [CsvIndex(36)]
-            public string CreditStatus { get; set; }
-
-            [CsvIndex(37)]
-            public DateTimeOffset DateUpdated { get; set; }
-
-            [CsvIgnore]
-            public List<MobileServiceModel> Services { get; } = new();
-
-            [CsvIndex(40)]
-            public string MarketingOptOut { get; set; }
-        }
+        [Index(40)]
+        [CsvIndex(40)]
+        public string MarketingOptOut { get; set; }
     }
 }
